f4db41df95dac4e85b4d3703538b5afa50f89673
SagarDas1124 <sagar092953@gmail.com>
2023-11-21 16:20:46 +0530

    2

--- src/operations/Read.jsx
+++ src/operations/Read.jsx
@@ -2,20 +2,13 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 import { Link } from "react-router-dom";
-import { Pagination } from "@mui/material";
 
 export const Read = () => {
-  const [data,setData]=useState([]);
-  const [page,setPage]=useState(1);
-  const rowPerPage=5;
-
-  const handlePage=(e,newPage)=>{
-    setPage(newPage);
-  }
+  const [data,setData]=useState([])
 
   const getData=async()=>{
     try{
-        const response= await axios.get("http://localhost:5000/api/v1/employee")
+        const response= await axios.get("https://crud-express-node.onrender.com/api/v1/employee")
         setData(response.data)
     }catch(error){
         console.log("error.message");
@@ -35,7 +28,7 @@ export const Read = () => {
 
   const handleDelete=async(id)=>{
     try{
-        await axios.delete(`http://localhost:5000/api/v1/employee/${id}`)
+        await axios.delete(`https://crud-express-node.onrender.com/api/v1/employee/${id}`)
         getData();
     }catch(error){
       console.log(error.message);
@@ -62,7 +55,7 @@ export const Read = () => {
         </thead>
         <tbody>
           {
-            data?.slice((page-1)*rowPerPage,(page-1)*rowPerPage+rowPerPage).map((eachData)=>{
+            data?.map((eachData)=>{
               return(
                 <tr key={eachData._id}>
                 <td className="text-center">{eachData.ename}</td>
@@ -81,14 +74,7 @@ export const Read = () => {
             })
           }
           
-        </tbody>     
-        <Pagination 
-          count={Math.ceil(data.length/rowPerPage)}
-          color="primary"
-          onChange={handlePage}
-          page={page}
-        />
-            
+        </tbody>        
       </table>     
     </>
   );
